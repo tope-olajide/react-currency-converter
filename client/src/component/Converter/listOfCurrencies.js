dde80a57c3f829cd7b2551d7ce313f20f626a09e
Temitope <tope_olajide@outlook.com>
2019-05-28 03:20:41 +0100

    Create listOfCurrencies file

---
+++ client/src/component/Converter/listOfCurrencies.js
@@ -0,0 +1,10 @@
+const listOfCurrencies =
+    [
+        { label: "Australia Dollar", value: "AUD" },
+        { label: "Great Britain Pound", value: "GBP" },
+        { label: "Euro", value: "EUR" },
+        { label: "Japan Yen", value: "JPY" },
+        { label: "Switzerland Franc", value: "CHF" },
+        { label: "USA Dollar", value: "USD" }
+      ];
+export default listOfCurrencies
