b28e7b42e608d2c9c12eda318795559328d94c74
Temitope <tope_olajide@outlook.com>
2019-05-28 03:19:37 +0100

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import CurrencyConverter from './component/Converter'
+function App() {
+  return (
+    <>
+<CurrencyConverter />
+    </>
+  );
+}
+
+export default App;
