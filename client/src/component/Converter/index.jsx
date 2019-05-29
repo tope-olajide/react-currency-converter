2d437f371fbd37f2b35e3f0a56a32413725ddc1c
Temitope <tope_olajide@outlook.com>
2019-05-29 22:21:59 +0100

    Update index.jsx

--- client/src/component/Converter/index.jsx
+++ client/src/component/Converter/index.jsx
@@ -1,38 +1,91 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import listOfCurrencies from "./listOfCurrencies";
 const CurrencyConverter = () => {
+  const [amountToConvert, setAmountToConvert] = useState(1);
   const [fromCurrency, setFromCurrency] = useState({
-    name: "Nigeria Naira",
-    value: "NGN"
-  });
-  const [toCurrency, setToCurrency] = useState({
-    label: "USA Dollar",
     value: "USD"
   });
-  const currencyList = listOfCurrencies.map(currency => {
-    return <option value={currency.value}  name={currency.label}>{currency.label}</option>;
+  const [toCurrency, setToCurrency] = useState({
+    value: "NGN"
   });
-  const handleFromCurrencyChange = (event)=>{
-    setFromCurrency({value:event.target.value});
-    console.log(fromCurrency)
-  }
+  const [isLoading, setIsloading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  useEffect(() => {
+    const handleCurrencyConversion = () => {
+      setIsloading(true)
+      setIsError(false)
+        axios
+          .get(
+            `https://free.currconv.com/api/v7/convert?q=${fromCurrency.value}_${
+              toCurrency.value
+            }&compact=ultra&apiKey=c219dc5827286852c542`
+          )
+          .then(response => {
+            setIsloading(false)
+            setIsError(false)
+            const { data } = response;
+            console.log(data);
+            console.log(Object.values(data)[0]);
+            const result = amountToConvert * Object.values(data)[0];
+            setConversionResults(result);
+          })
+          .catch(err => {
+            setIsError(true)
+            console.log("Error: ", err.message);
+          });
+       
+    };handleCurrencyConversion()
+  }, [amountToConvert, fromCurrency, toCurrency]);
+
+  const [conversionResults, setConversionResults] = useState("");
+  const currencyList = listOfCurrencies.map(currency => {
+    return (
+      <option value={currency.value} >
+        {currency.label}
+      </option>
+    );
+  });
+  const getFullCurrencyName = currencyAcronym => {
+    return listOfCurrencies.filter(currency => {
+      if (currency.value === currencyAcronym) {
+        return currency.value;
+      }
+      return false;
+    });
+  };
+
+  const handleInputChange = event => {
+    setAmountToConvert(event.target.value);
+  };
+  const handleFromCurrencyChange = event => {
+    setFromCurrency({ value: event.target.value });
+  };
+  const handleToCurrencyChange = event => {
+    setToCurrency({ value: event.target.value });
+  };
+  const baseCurrencyName = getFullCurrencyName((fromCurrency.value)[0].label);
+  const quoteCurrencyName = getFullCurrencyName((toCurrency.value)[0].label);
+
+
   return (
     <>
       <div class="container">
         <div class="converter-box">
           <h3 class="title">Currency Converter</h3>
+          <p>Enter Amount to Convert</p>
           <input
             class="text-area"
             placeholder="Enter Amount to Convert"
             type="text"
+            defaultValue="1"
+            onChange={handleInputChange}
           />
           <div class="selector-container">
             <div>
               <p>From</p>
               <select
                 class="selector"
-                name={fromCurrency.value}
                 value={fromCurrency.value}
                 onChange={handleFromCurrencyChange}
               >
@@ -41,12 +94,21 @@ const CurrencyConverter = () => {
             </div>
             <div>
               <p>To</p>
-              <select class="selector">{currencyList}</select>
+              <select
+                class="selector"
+                value={toCurrency.value}
+                onChange={handleToCurrencyChange}
+              >
+                {currencyList}
+              </select>
             </div>
           </div>
-          <p class="conversion_output">0</p>
+          <p class="conversion_output">{(isLoading)?"":conversionResults}</p>
           <p class="conversion_output-summary">
-            1 Trinidad/Tobago Dollar equals 2345.344 Euro
+            { (isLoading)? <i class="fa fa-spinner fa-spin" style={{"font-size":"40px"}}></i>:`${amountToConvert} ${baseCurrencyName} equals ${conversionResults} ${quoteCurrencyName}`
+            }<br />
+          {(isError)?`Unable to connect to the converter API, please check your network and try again`:""}
+          
           </p>
         </div>
       </div>
