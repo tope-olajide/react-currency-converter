04a01ffb9ac0d569e38295be4c4d2c56e8bbf256
Temitope <tope_olajide@outlook.com>
2019-05-29 22:22:06 +0100

    Update listOfCurrencies.js

--- client/src/component/Converter/listOfCurrencies.js
+++ client/src/component/Converter/listOfCurrencies.js
@@ -1,10 +1,12 @@
-const listOfCurrencies =
-    [
-        { label: "Australia Dollar", value: "AUD" },
-        { label: "Great Britain Pound", value: "GBP" },
-        { label: "Euro", value: "EUR" },
-        { label: "Japan Yen", value: "JPY" },
-        { label: "Switzerland Franc", value: "CHF" },
-        { label: "USA Dollar", value: "USD" }
-      ];
-export default listOfCurrencies
+const listOfCurrencies = [
+  { label: "Australia Dollar", value: "AUD" },
+  { label: "Great Britain Pound", value: "GBP" },
+  { label: "Euro", value: "EUR" },
+  { label: "Japan Yen", value: "JPY" },
+  { label: "Switzerland Franc", value: "CHF" },
+  { label: "USA Dollar", value: "USD" },
+  { label: "Nigeria Naira", value: "NGN" },
+  { label: "Afghanistan Afghani", value: "AFN" },
+  { label: "Albania Lek", value: "EUR" },
+];
+export default listOfCurrencies;
